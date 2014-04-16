85b2e86686a36f27819dcb1d1c3ec05013040e7f
Paul Lam <paul@quantisan.com>
2014-04-16 12:21:54 -0400

    prototype a callback verification, working on CLI

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/smtp"
+	"os"
+)
+
+func main() {
+	domain := os.Args[1]
+	email := os.Args[2]
+	mx, _ := net.LookupMX(domain)
+	host := mx[0].Host
+	log.Println("Found MX Host: " + host)
+	c, err := smtp.Dial(host + ":25")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := c.Hello("verify-email.org"); err != nil {
+		log.Fatal(err)
+	}
+	if err := c.Mail("check@verify-email.org"); err != nil {
+		log.Fatal(err)
+	}
+	if err := c.Rcpt(email); err != nil {
+		log.Println("Fail")
+	} else {
+		log.Println("Success")
+	}
+}
